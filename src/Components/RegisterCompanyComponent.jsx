b4cafe5d1aaf27096563b438880165de9669c8ab
wichien445 <wichien445@hotmail.com>
2023-06-13 11:49:32 +0700

    last update

--- src/Components/RegisterCompanyComponent.jsx
+++ src/Components/RegisterCompanyComponent.jsx
@@ -124,7 +124,24 @@ function RegisterCompanyComponent() {
                   onChange={inputValue('company')}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Flowbite" required/>
               </div>  
-              
+              <div>
+                <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900">Phone number</label>
+                <input 
+                  type="tel"
+                  id="phone"
+                  value={phone}
+                  onChange={inputValue('phone')}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="012-345-6789" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" required/>
+              </div>
+              <div>
+                <label htmlFor="website" className="block mb-2 text-sm font-medium text-gray-900">Company Size</label>
+                <input 
+                  type="text" 
+                  id="companysize"
+                  value={companysize}
+                  onChange={inputValue('companysize')}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Company Size" required/>
+              </div>
               <div>
                 <label htmlFor="visitors" className="block mb-2 text-sm font-medium text-gray-900">Country</label>
                 <input 
